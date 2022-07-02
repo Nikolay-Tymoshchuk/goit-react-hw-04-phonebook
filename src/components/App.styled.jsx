5ef30c0bcd62312b70b3ad98e7790cd6481ca2be
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-07-02 23:48:44 +0300

    created necessary files

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 300px;
+  height: max-content;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  font-size: 40;
+  margin-left: 2%;
+  margin-top: 2%;
+  color: var(--color-text-primary);
+  border-radius: 10px;
+  padding: 8px;
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.2);
+`;
+
+export const Title = styled.h1`
+  font-size: 40;
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 30;
+`;
